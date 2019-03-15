31d74fecec3cd5f1481a4e2e805114c1fc93395e
Sergey Neupokoev <Sergey@Sergeys-MacBook-Pro.local>
2019-03-15 19:28:13 +0400

    Setup the tableView

---
+++ splitter/ImagesAndNamesAndColors.swift
@@ -0,0 +1,24 @@
+//
+//  ImagesAndNamesAndColors.swift
+//  splitter
+//
+//  Created by Sergey Neupokoev on 15/3/19.
+//  Copyright © 2019 Sergey Neupokoev. All rights reserved.
+//
+
+import UIKit
+
+//images
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imagesArray = [camera, city, buffalo, bouqet, stand, urban]
+
+//names
+let namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STANDS","URBAN"]
+
+let colorsArray = [#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1),]
